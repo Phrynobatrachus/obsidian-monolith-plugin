2269b17eaf70cc5568b26e2b0ad5b42bd410d2ac
Cass Caplinger <hello@casscaplinger.me>
2023-02-26 22:48:37 -0500

    feat: implement simple Monolith interface
    Use Monolith and xdg-open via Node spawn() to save and access archived URLs.

--- main.ts
+++ main.ts
@@ -1,85 +1,90 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { spawn } from 'child_process';
+import { homedir } from 'os';
+import { App, MarkdownRenderChild, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface MonolithPluginSettings {
+	cliOpts: string[];
+	outputPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MonolithPluginSettings = {
+	cliOpts: ['--no-js', '--isolate'],
+	outputPath: homedir()
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MonolithPlugin extends Plugin {
+	settings: MonolithPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new SettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerMarkdownPostProcessor((element, context) => {
+			const links = element.querySelectorAll("a.archivedLink");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			for (let index = 0; index < links.length; index++) {
+				const link = links.item(index);
+				// @ts-ignore
+				context.addChild(new ArchiveLink(link));
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'archive-link',
+			name: 'Archive link',
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const potentialUrl = view?.editor.getSelection() || "";
+				const url = new URL(potentialUrl);
+
+				if (url) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						const outputFile = this.archiveLink(url);
+						const anchorString = `<a class="archivedLink" href="file://${this.settings.outputPath}/${outputFile}">(archived)</a>`;
+
+						// add link to output file
+						view?.editor.replaceSelection(`${url.toString()} ${anchorString}`)
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
 			}
 		});
+	}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+	archiveLink(url: URL) {
+		const path = url.pathname.split('/');
+		// trailing slash is falsy
+		const outputFile = `${path.pop() || path.pop()}.html`;
+		const baseArgs = [`-o${outputFile}`, url.toString()];
+		const args = (this.settings.cliOpts[0] !== '') ? [...this.settings.cliOpts, ...baseArgs] : baseArgs;
+		const spawnOpts = { cwd: this.settings.outputPath };
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		const monolith = spawn('monolith', args, spawnOpts);
+
+		monolith.stdout.on('data', (data) => {
+			console.log(data.toString());
+		});
+		monolith.stderr.on('data', (data) => {
+			console.log(data.toString());
+		});
+		monolith.on('close', (code) => {
+			if (code === 0) {
+				new Notice('Link archived!')
+			} else {
+				new Notice('Archiving failed, check console.')
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		return outputFile;
 	}
 
 	onunload() {
-
+		// maybe clean up links
 	}
 
 	async loadSettings() {
@@ -91,46 +96,61 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+export class ArchiveLink extends MarkdownRenderChild {
+	constructor(containerEl: HTMLElement) {
+		super(containerEl);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	openLink(e: Event) {
+		e.preventDefault();
+		// @ts-ignore
+		const browser = spawn('open', [this.containerEl.href]);
+
+		browser.stdout.on('data', (data) => {
+			console.log(data.toString());
+		});
+		browser.stderr.on('data', (data) => {
+			console.log(data.toString());
+		});
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	onload() {
+		this.containerEl.onclick = this.openLink.bind(this);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SettingTab extends PluginSettingTab {
+	plugin: MonolithPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MonolithPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Monolith plugin settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Flags')
+			.setDesc('Used when invoking Monolith.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('monolith --help')
+				.setValue(this.plugin.settings.cliOpts.join(' '))
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.cliOpts = value.split(' ');
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Output folder')
+			.setDesc('Where archived pages will be saved.')
+			.addText(text => text
+				.setPlaceholder('$HOME')
+				.setValue(this.plugin.settings.outputPath)
+				.onChange(async (value) => {
+					this.plugin.settings.outputPath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
